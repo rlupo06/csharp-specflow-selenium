c1cc8e4e667e2b257f8193ba52f7b9f62196bcdb
Rob Lupo <rlupo@tapqa.com>
2018-06-02 12:31:01 -0500

    Reorganizing project

--- SpecFlow.Selenium/Cucumber/Features/DemoTwo.feature.cs
+++ SpecFlow.Selenium/Cucumber/Features/DemoTwo.feature.cs
@@ -10,7 +10,7 @@
 // ------------------------------------------------------------------------------
 #region Designer generated code
 #pragma warning disable
-namespace SpecFlow.Selenium.Features
+namespace SpecFlow.Selenium.Cucumber.Features
 {
     using TechTalk.SpecFlow;
     
